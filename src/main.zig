116ca2038e0f5231d17dae50902a67809e7cf723
heli82 <89199260267den@gmail.com>
2024-09-06 15:43:30 +0500

    Action

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,9 @@ const Allocator = std.mem.Allocator;
 var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = false }){};
 const alloc = gpa.allocator();
 
+const stdin = std.io.getStdIn().reader();
+const stdout = std.io.getStdOut().writer();
+
 const Suit = enum(u2) {
     spades,
     hearts,
@@ -51,7 +54,7 @@ const Card = struct {
     }
 };
 
-pub fn generate_deck() error{OutOfMemory}!Arraylist(Card) {
+pub fn generate_deck() !Arraylist(Card) {
     const RndGen = std.Random.DefaultPrng;
     const time = std.time;
     var rng = RndGen.init(@as(u64, @abs(time.timestamp())));
@@ -70,11 +73,67 @@ pub fn generate_deck() error{OutOfMemory}!Arraylist(Card) {
     return deck;
 }
 
+pub fn draw(hand: *Arraylist(Card), deck: *Arraylist(Card), amount: u8) !void {
+    for (0..@min(amount, deck.capacity)) |_| {
+        try hand.append(deck.pop());
+    }
+}
+
 pub fn main() !void {
-    const deck = try generate_deck();
+    var deck = try generate_deck();
     defer deck.deinit();
-    
-    for (deck.items) |card| {
-        std.debug.print("{}\n", .{card});
+    var hand = Arraylist(Card).init(alloc);
+    defer hand.deinit();
+
+    var discards: u8 = 4;
+    var hands: u8 = 4;
+
+    try draw(&hand, &deck, 8);
+
+    while (hands > 0) {
+        try stdout.print("Your hand:\n", .{});
+        for (0.., hand.items) |i, card| {
+            try stdout.print("{d}: {}\n", .{ i, card });
+        }
+
+        // try stdout.print("{s}", .{std.mem.trim(u8, "\nHello\n", "\n")});
+        const Act = enum(u8) { Play = 1, Discard = 2 };
+
+        var line: [64]u8 = undefined;
+        const size = try stdin.read(&line);
+        const player_act = std.mem.trim(u8, line[0..size], "\n");
+
+        var splitted_act = std.mem.split(u8, player_act, " ");
+
+        var action: Act = undefined;
+        var selected_cards = Arraylist(Card).init(alloc);
+        defer selected_cards.deinit();
+
+        while (splitted_act.next()) |x| {
+            if (action != undefined) {
+                if (std.mem.eql(u8, x, "play")) {
+                    action = Act.Play;
+                } else if (std.mem.eql(u8, x, "discard")) {
+                    action = Act.Discard;
+                }
+            } else {
+                const select_index = std.fmt.parseInt(u8, x, 10) catch |err| switch (err) {
+                    error.Overflow => {
+                        try stdout.writeAll("Please enter a small positive number\n");
+                        continue;
+                    },
+                    error.InvalidCharacter => {
+                        try stdout.writeAll("Please enter a valid number\n");
+                        continue;
+                    },
+                };
+                try selected_cards.append(hand.items[select_index]);
+            }
+        }
+		
+		std.debug.print("{}, {}", .{action, selected_cards});
+
+        hands -= 1;
+        discards -= 1;
     }
 }
